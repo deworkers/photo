cd9ac9dbcea76ab23ce9f8ca3a6490308e1ba338
Евгений Беляев <z-be@yandex.ru>
2017-05-24 11:03:50 +0500

    main-page

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+/*
+ * Third party
+ */
+//= ../../bower_components/jquery/dist/jquery.js
+
+//= ../plugin/jquery-ui/jquery-ui.min.js
+
+//= ../plugin/owl.carousel/owl.carousel.min.js
+//= ../plugin/jquery.validate.min.js
+//= ../plugin/jquery.cycle.all.js
+
+/*
+ * Custom
+ */
+//= part/app.js
